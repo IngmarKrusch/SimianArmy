33800fa41e35e4a3cd5939170d6d1504fac77424
Ingmar Krusch <krusch@gmx.de>
2013-09-09 16:57:43 +0200

    moved TerminationStrategy interface

---
+++ src/main/java/com/netflix/simianarmy/basic/chaos/NoOpTerminationStrategy.java
@@ -0,0 +1,22 @@
+package com.netflix.simianarmy.basic.chaos;
+
+import java.rmi.RemoteException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vmware.vim25.mo.VirtualMachine;
+
+/**
+ * Only logs the termination of the given VirtualMachine but DOES NOTHING else!
+ *
+ * @author ingmar.krusch@immobilienscout24.de
+ */
+public class NoOpTerminationStrategy implements TerminationStrategy {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NoOpTerminationStrategy.class);
+
+    @Override
+    public void terminate(VirtualMachine virtualMachine) throws RemoteException {
+        LOGGER.info("skipped termination of " + virtualMachine.getName());
+    }
+}
